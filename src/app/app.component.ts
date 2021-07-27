3aa9e5e24b3bd0926e740357c8b56e31dc22a5e5
C0ZEN <geoffrey.testelin@gmail.com>
2021-07-27 21:20:38 +0200

    4 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+  
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, AbstractControl, AsyncValidatorFn } from '@ngneat/reactive-forms';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+interface IForm {
+  count: number;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  public form: FormGroup<IForm> = new FormGroup<IForm>({
+    count: new FormControl<number | null>(
+      null,
+      undefined,
+      this._getCountValidators()
+    )
+  });
+
+  private _getCountValidators(): AsyncValidatorFn[] {
+    return [this._getCountValidator()];
+  }
+
+  private _getCountValidator(): (
+    control: AbstractControl<number | null>
+  ) => Observable<null> {
+    return (): Observable<null> => {
+      return of(null);
+    };
+  }
 }
